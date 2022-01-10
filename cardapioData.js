9c3df54da44b13cd1f3e239169f1003f47a59e18
Hyan Lopes <hyanlopess97@gmail.com>
2022-01-10 14:46:57 -0300

    create

--- cardapioData.js
+++ cardapioData.js
@@ -3,48 +3,48 @@ let cardapio = [
       id: 0,
       nome: 'X-Burger',
       preco: 25,
-      peso: '330g'
+      peso: 330
   },
   {
       id: 1,
       nome: 'Pizza',
       preco: 58,
-      peso: '130g'
+      peso: 130
   },
   {
       id: 2,
       nome: 'Sushi',
       preco: 48,
-      peso: '500g'
+      peso: 500
   },
   {
       id: 3,
       nome: 'Esfirra',
       preco: 8,
-      peso: '85g'
+      peso: 85
   },
   {
       id: 4,
       nome: 'Lasanha',
       preco: 22,
-      peso: '1100g'
+      peso: 1100
   },
   {
       id: 5,
       nome: 'Refrigerante',
       preco: 10,
-      peso: '2000ml'
+      peso: 2000
   },
   {
       id: 6,
       nome: 'Suco de Uva',
       preco: 5,
-      peso: '600ml'
+      peso: 600
   },
   {
       id: 7,
       nome: 'Suco de Abacaxi',
       preco: 5,
-      peso: '600ml'
+      peso: 600
   }
 ]
--- script.js
+++ script.js
@@ -10,32 +10,69 @@ let pedido = {
 
 // Função será responsável por atualizar a variável nomes
 function definirNome(minhaListaDeProdutos){
-
+  for (let i = 0; i < minhaListaDeProdutos.length ; i++){
+      pedido.nomes.push(cardapio[minhaListaDeProdutos[i]]['nome'])
+  }
+  return pedido
 }
+// console.log(definirNome([1,2,6,5]))
+
 
 // Função será responsável por atualizar a variável precoTotal
 function definirPrecoTotal(minhaListaDeProdutos){
+  for (let i = 0; i < minhaListaDeProdutos.length; i++){
+    pedido.precoTotal += cardapio[minhaListaDeProdutos[i]].preco
+  }
 }
+// console.log(definirPrecoTotal([1,2,6,5]))
 
 // Função será responsável por atualizar a variável pesos
-function definirGramasEMililitros (produtos){
-
+function definirGramasEMililitros (minhaListaDeProdutos){
+  let mililitros = 0
+  let gramas = 0
+  for(let i = 0; i < minhaListaDeProdutos.length; i++){
+    if(minhaListaDeProdutos[i]>5){
+      gramas += cardapio[minhaListaDeProdutos[i]].peso
+    } else {
+      mililitros += cardapio[minhaListaDeProdutos[i]].peso
+    }
+  }
+  pedido.pesos.gramas = gramas
+  pedido.pesos.mililitros=mililitros
 }
+// console.log(definirGramasEMililitros([1,2,6,5]))
 
 // Função será responsavel por atualizar a variavel minhaListaDeProdutos 
-function encontrarItensPeloId(idList){
+// function encontrarItensPeloId(idList){
+//   let id = 0
+//   let filtro = cardapio.filter(function(elemento,indice){
+//     // console.log(elemento)
+//     id = idList[0]
+//     return elemento.id
+//   })
+//   // let result = []
+//   // for (let i = 0; i<idList.length; i++){
+//   //   // result.push(idList[i])
+//   //   return idList[i]
+//   // }
+//   // return result
+//   return filtro
 
-}
+// }
+// encontrarItensPeloId([1])
+
 
 // Função será responsável pela apresentação do pedido. Obs: usar console.log() invés return na string criada.
 function apresentacao() {
-
+  document.write(`Pedidos: ${pedido.nomes} <br>`)
+  document.write(`Preço total do seu pedido: R$ ${pedido.precoTotal},00 <br>`)
+  document.write(`O peso dos seus lanches é ${pedido.pesos.mililitros}, ${pedido.pesos.gramas}kgs`)
 }
 
 // Função principal da nossa aplicação, será responsável por chamar as funções.
 function principal(idDosProdutosComprados){
   // objetos que serão filtrados conforme o id dos itens. Obs: esse sera nosso array de objetos.
-  const minhaListaDeProdutos = encontrarItensPeloId(idDosProdutosComprados);
+  const minhaListaDeProdutos = idDosProdutosComprados;
 
   // atualizar nomes
   definirNome(minhaListaDeProdutos);
@@ -50,4 +87,5 @@ function principal(idDosProdutosComprados){
   apresentacao()
 }
 
-principal([1, 3, 7])
+
+principal([1,2,3,4,5,7])
